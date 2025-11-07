fe916f78b416c6462f788aa43a6bc111b0e7cd82
GOGEON <g26963@naver.com>
2025-11-07 11:20:28 +0900

    사용하지 않는 코드 정리

--- backend/migrations/add_dates_to_todo.sql
+++
@@ -1,7 +0,0 @@
--- Add date columns to todo_items table
-ALTER TABLE todo_items ADD COLUMN due_date DATETIME NULL;
-ALTER TABLE todo_items ADD COLUMN reminder_date DATETIME NULL;
-
--- Create indexes for faster date filtering
-CREATE INDEX idx_todo_items_due_date ON todo_items(due_date);
-CREATE INDEX idx_todo_items_reminder_date ON todo_items(reminder_date);
